f85008369a8145949a85d5ced34a5f043adb9530
weighan <weighanlee@gmail.com>
2019-01-13 01:13:56 -0800

    rpm initial implementation

--- app/src/main/java/com/example/katsu/machinisthelper/RPM.java
+++ app/src/main/java/com/example/katsu/machinisthelper/RPM.java
@@ -2,12 +2,52 @@ package com.example.katsu.machinisthelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RPM extends AppCompatActivity {
+
+    int sfm, diameter;
+
+    EditText InLeft = (EditText) findViewById(R.id.InLeft);
+    EditText InRight = (EditText) findViewById(R.id.InRight);
+    TextView ans = (TextView) findViewById(R.id.answer_result);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rpm);
+
+
+
+
+    }
+
+
+    public void calculate(View view){
+
+        if(isNumeric(InLeft.getText().toString())){
+            sfm = Integer.parseInt(InLeft.getText().toString());
+        }
+        if(isNumeric(InRight.getText().toString())) {
+            diameter = Integer.parseInt(InRight.getText().toString());
+        }
+
+        double result = sfm * (12.0/Math.PI) / diameter;
+        ans.setText(Double.toString(result));
+
+    }
+
+    public static boolean isNumeric(String string){
+        try{
+            double d = Double.parseDouble(string);
+        }
+        catch(NumberFormatException nfe){
+            return false;
+        }
+        return true;
     }
 }
+
+
